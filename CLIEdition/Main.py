988b1bcffb2860c27c7f099dbc9788ea66527b03
Ririto Ninigaya(Ryo Tabuki) <riritoninigaya@hotmail.com>
2024-06-07 20:22:57 +0300

    Add files via upload

---
+++ CLIEdition/Main.py
@@ -0,0 +1,20 @@
+import pytube as youtube_py
+
+def Main():
+    yt_down = input("Please Write YouTube Link: ")
+
+    video_link = youtube_py.YouTube(str(yt_down))
+
+    videol = video_link.streams.filter(file_extension="mp4") 
+
+    res720 = videol.filter(audio_codec="MP3")
+
+    if(res720):
+        print("This Resolution Is Founded... Downloading...")
+        res720[0].download()
+    else:
+        print("This Resolution Is Not Founded... Pls Try Another Video")
+        return
+
+if __name__ == "__main__":
+    Main()
